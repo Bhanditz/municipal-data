a74e9e2a32bd261222ae9f5333ccfbe7083235b2
JD Bothma <jbothma@gmail.com>
2016-11-28 09:23:47 +0200

    count matches and non-matches

--- municipal_finance/management/commands/spending_detail.py
+++ municipal_finance/management/commands/spending_detail.py
@@ -2,9 +2,11 @@ from django.core.management.base import BaseCommand
 import csv
 from pprint import pprint
 import re
+from collections import defaultdict
 
 REGEXES = {
     'decimal_lon_lat': '^(\d+.\d+)[ ;]+(-\d+.\d+)$',
+    'known_unfilled': '^(N/A|0)?$',
 }
 
 FIELDNAMES = [
@@ -33,16 +35,22 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         compiled_rxs = compile_regexes()
+        counts = defaultdict(int)
         with open(options['infile'], 'rb') as f:
             reader = csv.DictReader(f, fieldnames=FIELDNAMES)
             for row in reader:
-                for key, value in compiled_rxs.iteritems():
-                    match = value.match(row['coordinates'])
+                match = False
+                for regex_name, regex in compiled_rxs.iteritems():
+                    match = regex.match(row['coordinates'])
                     if match:
-                        print("%s [%s] %s %s" % (row['demarcation_code'], row['coordinates'], row['proj_desc'], row['asset_class']))
-                        pprint(match.groups())
+                        counts[regex_name] += 1
+                        #print("%s [%s] %s %s" % (row['demarcation_code'], row['coordinates'], row['proj_desc'], row['asset_class']))
+                        #pprint(match.groups())
                         break
-
+                if not match:
+                    print("%s [%s] %s %s" % (row['demarcation_code'], row['coordinates'], row['proj_desc'], row['asset_class']))
+                    counts['no_match'] += 1
+        pprint(counts)
 
 def compile_regexes():
     compileds = {}
