5fb3a01a2ca06652f23cb78cc5d149a1d80393d2
JD Bothma <jbothma@gmail.com>
2016-11-28 12:50:28 +0200

    handle ETH

--- municipal_finance/management/commands/spending_detail.py
+++ municipal_finance/management/commands/spending_detail.py
@@ -8,9 +8,11 @@ REGEXES = {
     'decimal_lon_lat': '^(?P<lon>\d+[\.,]\d+)[\s;,]+(?P<lat>-\d+[\.,]\d+)$',
     'decimal_lat_lon_labeled': '^Latitude: (?P<lat>-\d+\.\d+)[ |;,]+Longt?itude: (?P<lon>\d+\.\d+)$',
     'decimal_lat_lon': '^(?P<lat>-\d+[\.,]\d+)[\s;,]+(?P<lon>\d+[\.,]\d+)$',
+    'decimal_lat_lon_unseparated': '^(?P<lat>-\d+[\.,]\d+)(?P<lon>\d{2}[\.,]\d+)$',
     'decimal_lat_neg_lon': '^(?P<lon>3\d+[\.,]\d+)[ ;,-]+(?P<lat>2\d+[\.,]\d+)$',
     'decimal_Slat_neg_Elon': '^S(?P<lat_neg>\d+[\.,]\d+)[ ;]+E(?P<lon>\d+[\.,]\d+)$',
-    'dms_lat_negS_lonE': '^(?P<lat_deg_neg>\d+)[\xB0\xBA] ?(?P<lat_min>\d+)\' ?(?P<lat_sec>\d+\.\d+)"S *(?P<lon_deg>\d+)[\xB0\xBA] ?(?P<lon_min>\d+)\' ?(?P<lon_sec>\d+\.\d+)"E$',
+    'dms_lat_negS_lonE': '^(?P<lat_deg_neg>\d+)[\xB0\xBA] ?(?P<lat_min>\d+)\' ?(?P<lat_sec>\d+\.\d+)("|'') ?S *(?P<lon_deg>\d+)[\xB0\xBA] ?(?P<lon_min>\d+)\' ?(?P<lon_sec>\d+\.\d+)("|'') ?E$',
+    'dms_lat_negS_lonE': '^(?P<lon_deg>\d+)[\xB0\xBA] ?(?P<lon_min>\d+)\' ?(?P<lon_sec>\d+\.\d+)("|'') ?E *(?P<lat_deg_neg>\d+)[\xB0\xBA] ?(?P<lat_min>\d+)\' ?(?P<lat_sec>\d+\.\d+)("|'') ?S$',
     'dms_lon_lat': '^(?P<lon>\d+[\xB0\xBA] ?\d+\' ?\d+") *(?P<lat>-\d+[\xB0\xBA] ?\d+\' ?\d+")$',
     'dms_lat_negS_lonE_comma': '^(?P<lat_deg_neg>\d+)\.(?P<lat_min>\d+)\.(?P<lat_sec>\d+,\d+) ?S *(?P<lon_deg>\d+)\.(?P<lon_min>\d+)\.(?P<lon_sec>\d+,\d+) ?E$',
     'dms_lat_negS_lonE_qmark': '^(?P<lat_deg_neg>\d+)\?(?P<lat_min>\d+)\'(?P<lat_sec>\d+\.\d+)(''|")S *(?P<lon_deg>\d+)\?(?P<lon_min>\d+)\'(?P<lon_sec>\d+\.\d+)(''|")E$',
@@ -44,6 +46,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         compiled_rxs = compile_regexes()
         counts = defaultdict(int)
+        muni_counts = defaultdict(int)
         with open(options['infile'], 'rb') as f:
             reader = csv.DictReader(f, fieldnames=FIELDNAMES)
             for row in reader:
@@ -59,6 +62,10 @@ class Command(BaseCommand):
                 if not match:
                     pprint("%s [%s] %s %s" % (row['demarcation_code'], coordstr, row['proj_desc'], row['asset_class']))
                     counts['no_match'] += 1
+                    muni_counts[row['demarcation_code']] += 1
+        print
+        for muni, count in sorted(muni_counts.iteritems(), key=lambda x: x[1], reverse=True)[0:5]:
+            print("%s\t%s" % (muni, count))
         print
         total = 0
         for key in sorted(counts.keys()):
